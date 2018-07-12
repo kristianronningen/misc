1533b8f2631fd7f3deb244b33a1395dc30d481f1
Kristian Rønningen <kr@nordkappnett.no>
2018-07-12 09:15:52 +0200

    Script to install the extensions I use

---
+++ vscode/install-extension.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension eamodio.gitlens
+code --install-extension jtavin.ldif
+code --install-extension stkb.rewrap
+code --install-extension vscoss.vscode-ansible
+code --install-extension wholroyd.jinja
